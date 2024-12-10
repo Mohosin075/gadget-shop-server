7c7bc249e675a071cf4ce004e1a578e5e7018e77
Mohosin <mohosinali075@gmail.com>
2024-12-10 23:20:31 +0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+const app = express();
+const port = 3000;
+
+// middleware
+require("dotenv").config();
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fudiykq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+run();
+
+app.get("/", (req, res) => {
+  res.send("server is  running...!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
